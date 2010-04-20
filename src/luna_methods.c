26ad4c52384c3c6f080d43c3784b3d1611c26183
Rod Whitby <rod@whitby.id.au>
2010-04-20 17:52:45 +0930

    Skeleton for govnah service

---
+++ src/luna_methods.c
@@ -0,0 +1,53 @@
+/*=============================================================================
+ Copyright (C) 2010 WebOS Internals <support@webos-internals.org>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ =============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "luna_service.h"
+#include "luna_methods.h"
+
+//
+// A dummy method, useful for unimplemented functions or as a status function.
+// Called directly from webOS, and returns directly to webOS.
+//
+bool dummy_method(LSHandle* lshandle, LSMessage *message, void *ctx) {
+  LSError lserror;
+  LSErrorInit(&lserror);
+
+  if (!LSMessageReply(lshandle, message, "{\"returnValue\": true}", &lserror)) goto error;
+
+  return true;
+ error:
+  LSErrorPrint(&lserror, stderr);
+  LSErrorFree(&lserror);
+ end:
+  return false;
+}
+
+LSMethod luna_methods[] = {
+  { "status",	dummy_method },
+  { 0, 0 }
+};
+
+bool register_methods(LSPalmService *serviceHandle, LSError lserror) {
+  return LSPalmServiceRegisterCategory(serviceHandle, "/", luna_methods,
+				       NULL, NULL, NULL, &lserror);
+}
