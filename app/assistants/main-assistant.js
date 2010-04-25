20ae520d1f5a46d22cc509484ca4268296b9f191
oil <brandonvb@gmail.com>
2010-04-24 23:44:09 -0700

    re-did main scene with smaller graphs

--- app/assistants/main-assistant.js
+++ app/assistants/main-assistant.js
@@ -45,7 +45,9 @@ MainAssistant.prototype.setup = function()
 	this.subTitleElement =	this.controller.get('subTitle');
 	this.governorRow =		this.controller.get('governorRow');
 	this.governorCurrent =	this.controller.get('governorCurrent');
-	this.frequencyCurrent =	this.controller.get('frequencyCurrent');
+	this.freqCurrent =	this.controller.get('freqCurrent');
+	this.tempCurrent =	this.controller.get('tempCurrent');
+	this.loadCurrent =	this.controller.get('loadCurrent');
 	
 	// set version string random subtitle
 	this.titleElement.innerHTML = Mojo.Controller.appInfo.title;
@@ -61,28 +63,25 @@ MainAssistant.prototype.setup = function()
 	
 	this.controller.listen(this.governorRow, Mojo.Event.tap, this.governorTapHandler);
 		
-	
-	this.temps = [];
-	this.freqs = [];
+	this.data = $H();
 	this.rate = 1000;
 	
-	this.canvasElement = this.controller.get('graphCanvas');
-	this.scaleElement = this.controller.get('scale');
-	this.scaleElement.hide();
-	this.vertTop = this.controller.get('vertTop');
-	this.vertBot = this.controller.get('vertBot');
-	this.vertTopCount = this.controller.get('vertTopCount');
-	this.vertBotCount = this.controller.get('vertBotCount');
+	//this.canvasElement = this.controller.get('graphCanvas');
+	//this.scaleElement = this.controller.get('scale');
+	//this.scaleElement.hide();
+	//this.vertTop = this.controller.get('vertTop');
+	//this.vertBot = this.controller.get('vertBot');
+	//this.vertTopCount = this.controller.get('vertTopCount');
+	//this.vertBotCount = this.controller.get('vertBotCount');
 	
 	this.tempGraph = new lineGraph
 	(
-		this.canvasElement,
+		this.controller.get('tempCanvas'),
 		{
-			height: 100,
+			height: 30,
 			width: 320
 		}
-	);
-	
+	)
 	this.freqGraph = new lineGraph
 	(
 		this.controller.get('freqCanvas'),
@@ -91,19 +90,42 @@ MainAssistant.prototype.setup = function()
 			width: 320
 		}
 	);
+	this.loadGraph = new lineGraph
+	(
+		this.controller.get('loadCanvas'),
+		{
+			height: 30,
+			width: 320
+		}
+	);
+	/*
+	this.timeGraph = new lineGraph
+	(
+		this.controller.get('timeCanvas'),
+		{
+			height: 30,
+			width: 320
+		}
+	);
+	*/
 	
+	/*
 	this.tempGraphZoom = 0;
 	this.tempGraphZoomLevels = ['second','5seconds','10seconds','15seconds','30seconds','min'];
 	this.tempGraphPinching = false;
 	this.tempGraphPinchingZoom = this.tempGraphZoom;
+	*/
 	
 	this.timerHandler = this.timerFunction.bind(this);
-	this.tempHandler = this.onTemp.bindAsEventListener(this);
-    this.freqHandler =	this.freqHandler.bindAsEventListener(this);
+	this.tempHandler = this.tempHandler.bindAsEventListener(this);
+    this.freqHandler = this.freqHandler.bindAsEventListener(this);
+    this.loadHandler = this.loadHandler.bindAsEventListener(this);
+    this.timeHandler = this.timeHandler.bindAsEventListener(this);
 		
 	//this.timer = setInterval(this.timerHandler, this.rate);
 	this.timerHandler();
 	
+	/*
     this.gestureStartHandler =	this.gestureStartHandler.bindAsEventListener(this);
     this.gestureChangeHandler =	this.gestureChangeHandler.bindAsEventListener(this);
     this.gestureEndHandler =	this.gestureEndHandler.bindAsEventListener(this);
@@ -119,7 +141,7 @@ MainAssistant.prototype.setup = function()
 	this.controller.listen(this.canvasElement, Mojo.Event.dragStart,	this.dragStartHandler);
 	this.controller.listen(this.canvasElement, Mojo.Event.dragging,		this.draggingHandler);
 	this.controller.listen(this.canvasElement, Mojo.Event.dragEnd,		this.dragEndHandler);
-	
+	*/	
 };
 
 MainAssistant.prototype.onGovernor = function(payload)
@@ -134,104 +156,153 @@ MainAssistant.prototype.governorTap = function(event)
 	this.controller.stageController.pushScene('governor', this.currentGovernor);
 }
 
+MainAssistant.prototype.tempHandler = function(payload)
+{
+	if (payload.returnValue) 
+	{
+		var timestamp = Math.round(new Date().getTime()/1000.0);
+		var value = parseInt(payload.value);
+		
+		AppAssistant.updateIcon(value);
+		this.iconElement.className = 'icon temp-' + value;
+		this.tempCurrent.innerHTML = value + '<div class="unit">&deg;C</div>';
+		
+		var dataObj = this.data.get(timestamp)
+		if (!dataObj) dataObj = {};
+		if (!dataObj.temp)
+		{
+			dataObj.temp = {total:value, count:1, value:value};
+		}
+		if (dataObj.temp && dataObj.temp.count)
+		{
+			dataObj.temp.total = dataObj.temp.total + value;
+			dataObj.temp.count++;
+			dataObj.temp.value = (dataObj.temp.total / dataObj.temp.count);
+		}
+		this.data.set(timestamp, dataObj);
+	}
+};
 MainAssistant.prototype.freqHandler = function(payload)
 {
-	this.frequencyCurrent.innerHTML = (parseInt(payload.value) / 1000) + '<div class="unit">MHz</div>';
-	//alert ('-----');
-	//for (p in payload) alert(p+":"+payload[p]);
+	if (payload.returnValue) 
+	{
+		var timestamp = Math.round(new Date().getTime()/1000.0);
+		var value = parseInt(payload.value);
+	
+		this.freqCurrent.innerHTML = (value / 1000) + '<div class="unit">MHz</div>';
+		
+		var dataObj = this.data.get(timestamp)
+		if (!dataObj) dataObj = {};
+		if (!dataObj.freq)
+		{
+			dataObj.freq = {total:value, count:1, value:value};
+		}
+		if (dataObj.freq && dataObj.freq.count)
+		{
+			dataObj.freq.total = dataObj.freq.total + value;
+			dataObj.freq.count++;
+			dataObj.freq.value = (dataObj.freq.total / dataObj.freq.count);
+		}
+		this.data.set(timestamp, dataObj);
+	}
+}
+MainAssistant.prototype.loadHandler = function(payload)
+{
+	if (payload.returnValue) 
+	{
+		var timestamp = Math.round(new Date().getTime()/1000.0);
+		var valueArray = String(payload.stdOut).split(' ');
+		var value = parseFloat(trim(valueArray[0]));
+		
+		this.loadCurrent.innerHTML = valueArray[0] + ' ' + valueArray[1] + ' ' + valueArray[2];
+		
+		var dataObj = this.data.get(timestamp)
+		if (!dataObj) dataObj = {};
+		if (!dataObj.load)
+		{
+			dataObj.load = {total:value, count:1, value:value};
+		}
+		if (dataObj.load && dataObj.load.count)
+		{
+			dataObj.load.total = dataObj.load.total + value;
+			dataObj.load.count++;
+			dataObj.load.value = (dataObj.load.total / dataObj.load.count);
+		}
+		this.data.set(timestamp, dataObj);
+	}
+}
+MainAssistant.prototype.timeHandler = function(payload)
+{
+	/*
+	alert ('***** TIME');
+	for (p in payload) alert(p+":"+payload[p]);
+	
+	
+	if (payload.returnValue) 
+	{
+		var valueArray = payload.stdOut.split(',');
+		
+		alert(value);
+		
+	}
+	*/
 }
 
 MainAssistant.prototype.timerFunction = function()
 {
+	this.renderGraph();
+	
 	service.get_omap34xx_temp(this.tempHandler);
 	service.get_scaling_cur_freq(this.freqHandler);
+	service.get_proc_loadavg(this.loadHandler);
+	//service.get_time_in_state(this.timeHandler);
 	
 	this.timer = setTimeout(this.timerHandler, this.rate);
 };
 
-MainAssistant.prototype.onTemp = function(payload)
-{
-	if (payload.returnValue) {
-		//alert(payload.value);
-		AppAssistant.updateIcon(payload.value);
-		this.iconElement.className = 'icon temp-' + payload.value;
-	
-		this.temps.push({date:new Date(), value:payload.value})
-		this.renderGraph();
-	}
-};
 
 MainAssistant.prototype.renderGraph = function()
 {
 	
 	this.tempGraph.clearLines();
+	this.freqGraph.clearLines();
+	this.loadGraph.clearLines();
 	
-	var tmpData = [];
+	var tempData = [];
+	var freqData = [];
+	var loadData = [];
 	
-	var avg = false;
-	var points = 180;
+	var avg = 1;
+	var points = 80;
 	
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == 'second') avg = 1;
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == '5seconds') avg = 5;
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == '10seconds') avg = 10;
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == '15seconds') avg = 15;
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == '30seconds') avg = 30;
-	if (this.tempGraphZoomLevels[this.tempGraphPinchingZoom] == 'min') avg = 60;
-	
-	var avgData = $H();
-	for (var t = 0; t < this.temps.length; t++)
+	var keys = this.data.keys();
+	var start = 0;
+	if (keys.length > points) start = keys.length - points;
+	for (var k = start; k < keys.length; k++)
 	{
-		var avgTime = Math.round(this.temps[t].date.getTime()/1000);
-		avgTime = avgTime - (avgTime % avg);
+		var dataObj = this.data.get(keys[k]);
 		
-		if (tmpObj = avgData.get(avgTime))
-		{
-			tmpObj.total = tmpObj.total + this.temps[t].value;
-			tmpObj.count++;
-		}
-		else
-		{
-			var tmpObj =
-			{
-				total: this.temps[t].value,
-				count: 1
-			}
-		}
+		if (dataObj.temp)
+			tempData.push({key: keys[k], value: dataObj.temp.value});
 		
-		avgData.set(avgTime, tmpObj);
+		if (dataObj.freq)
+			freqData.push({key: keys[k], value: dataObj.freq.value});
+		
+		if (dataObj.load)
+			loadData.push({key: keys[k], value: dataObj.load.value});
 	}
 	
-	var tmpKeys = avgData.keys();
-	if (tmpKeys.length < points)
-	{
-		for (var t = 0; t < points - (tmpKeys.length % points); t++)
-		{
-			tmpData.push(false);
-		}
-	}
-	for (t = (tmpKeys.length > points ? tmpKeys.length - points : 0); t < tmpKeys.length; t++)
-	{
-		var tmpObj = avgData.get(tmpKeys[t]);
-		tmpData.push(tmpObj.total / tmpObj.count);
-	}
-	
-	this.tempGraph.setLine(tmpData, {});
-	
+	this.tempGraph.setLine(tempData, {strokeStyle: "rgba(153, 255, 153, .3)", fillStyle: "rgba(153, 255, 153, .1)"});
+	this.freqGraph.setLine(freqData, {strokeStyle: "rgba(255, 153, 153, .3)", fillStyle: "rgba(255, 153, 153, .1)"});
+	this.loadGraph.setLine(loadData, {strokeStyle: "rgba(153, 153, 255, .3)", fillStyle: "rgba(153, 153, 255, .1)"});
 	
 	this.tempGraph.render();
-	
-	
-	/*
-	this.freqGraph.clearLines();
-	this.freqGraph.setLine(tmpData, {fillStyle: "rgba(170, 170, 170, .2)", lineWidth: 1});
 	this.freqGraph.render();
-	*/
-	
-	this.vertTopCount.innerHTML = Math.round(this.tempGraph.lines[0].vertical.top) + '&deg;';
-	this.vertBotCount.innerHTML = Math.round(this.tempGraph.lines[0].vertical.bottom) + '&deg;';
+	this.loadGraph.render();
 	
 };
 
+/*
 MainAssistant.prototype.gestureStartHandler = function(event)
 {
 	this.scaleElement.show();
@@ -302,6 +373,7 @@ MainAssistant.prototype.draggingHandler = function(event)
 MainAssistant.prototype.dragEndHandler = function(event)
 {
 };
+*/
 
 MainAssistant.prototype.activate = function(event)
 {
--- app/models/line-graph.js
+++ app/models/line-graph.js
@@ -11,6 +11,7 @@ function lineGraph(graph, options)
 	this.defaultLine =
 	{
 		strokeStyle: "rgba(0, 0, 0, 1)",
+		fillStyle: false,
 		lineWidth: 2
 	};
 	
@@ -29,17 +30,12 @@ lineGraph.prototype.setLine = function(data, options)
 		{
 			if (data[d] !== false)
 			{
-				if (top < data[d]) top = data[d];
-				if (bottom > data[d]) bottom = data[d];
+				if (top < data[d].value) top = data[d].value;
+				if (bottom > data[d].value) bottom = data[d].value;
 			}
 		}
-		
-		var split = top - bottom;
-		if (split < 5) // hard 5 minimum?
-		{
-			top = top + (5 - (split % 5));
-		}
 	}
+	
 	var tmpLine =
 	{
 		data: data,
@@ -49,6 +45,7 @@ lineGraph.prototype.setLine = function(data, options)
 			bottom: options.bottom ? options.bottom : bottom
 		},
 		strokeStyle: options.strokeStyle ? options.strokeStyle : this.defaultLine.strokeStyle,
+		fillStyle: options.fillStyle ? options.fillStyle : this.defaultLine.fillStyle,
 		lineWidth: options.lineWidth ? options.lineWidth : this.defaultLine.lineWidth
 	};
 	
@@ -66,31 +63,43 @@ lineGraph.prototype.render = function()
 	
 	for (var line = 0; line < this.lines.length; line++)
 	{
-	
-		this.canvas.fillStyle = "rgba(170, 170, 170, .15)";
-		for (var v = 1; v < (this.lines[line].vertical.top - this.lines[line].vertical.bottom); v = v + 2)
-		{
-			this.canvas.fillRect(0, (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)) * v,
-								this.width, (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)));
-		}
-		
 		this.canvas.strokeStyle = this.lines[line].strokeStyle;
+		this.canvas.fillStyle = this.lines[line].fillStyle;
 		this.canvas.lineWidth = this.lines[line].lineWidth;
 		
 		var segmentLength = this.width / this.lines[line].data.length;
 		
-		var last = this.lines[line].data[0];
-		for (var d = 0; d < this.lines[line].data.length; d++)
+		var first = false
+		var start = 0;
+		while (first === false && start < this.lines[line].data.length)
 		{
-			var crnt = this.lines[line].data[d];
-			if (crnt !== false && last !== false)
+			first = this.lines[line].data[start];
+			start++;
+		}
+		
+		if (start < this.lines[line].data.length)
+		{
+			this.canvas.beginPath();
+			this.canvas.moveTo(0, this.height - (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)) * (first.value - this.lines[line].vertical.bottom));
+			
+			var last = first;
+			for (var d = start; d < this.lines[line].data.length; d++)
 			{
-				this.canvas.beginPath();
-				this.canvas.moveTo(d * segmentLength, this.height - (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)) * (last - this.lines[line].vertical.bottom));
-				this.canvas.lineTo((d * segmentLength) + segmentLength, this.height - (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)) * (crnt - this.lines[line].vertical.bottom));
-				this.canvas.stroke();
+				var crnt = this.lines[line].data[d];
+				if (crnt !== false)
+				{
+					this.canvas.lineTo((d * segmentLength) + segmentLength, this.height - (this.height / (this.lines[line].vertical.top - this.lines[line].vertical.bottom)) * (crnt.value - this.lines[line].vertical.bottom));
+					last = crnt;
+				}
 			}
-			last = crnt;
+			
+			if (this.lines[line].fillStyle)
+			{
+				this.canvas.lineTo(this.width+5, this.height+5);
+				this.canvas.lineTo(-5, this.height+5);
+				this.canvas.fill();
+			}
+			this.canvas.stroke();
 		}
 	}
 	
