506b7852a898d20c155f63ae522e8894c80eea2e
oil <brandonvb@gmail.com>
2010-10-26 00:46:01 -0700

    graph code update back from lumberjack

--- app/models/line-graph.js
+++ app/models/line-graph.js
@@ -10,10 +10,11 @@
 
 */
 
-function lineGraph(element, options)
+function lineGraph(element, options, labels)
 {
 	this.element = element;
-	this.canvas = this.element.getContext('2d');
+	this.labels  = labels;
+	this.canvas  = this.element.getContext('2d');
 	this.options = options || {};
 	
 	this.options = this.merge(options,
@@ -28,10 +29,15 @@ function lineGraph(element, options)
 		yaxis:
 		{
 			min:		null,
-			max:		null
+			max:		null,
+			tics:		false,
+			ticStroke:	"rgba(0, 0, 0, .5)",
+			ticFill:	false,
+			ticWidth:	1
 		},
 		padding:
 		{
+			clear:		false,
 			top:		0,
 			bottom:		0,
 			left:		0,
@@ -117,6 +123,53 @@ lineGraph.prototype.prepareLines = function()
 	this.xScale = this.drawWidth / (this.xaxis.max - this.xaxis.min);
 	this.yScale = this.drawHeight / (this.yaxis.max - this.yaxis.min);
 };
+lineGraph.prototype.renderTics = function()
+{
+	this.canvas.strokeStyle = this.options.yaxis.ticStroke;
+	this.canvas.fillStyle =   this.options.yaxis.ticFill;
+	this.canvas.lineWidth =   this.options.yaxis.ticWidth;
+	
+	this.yaxis.max = this.getBetterMaxY(this.yaxis.max);
+	this.yScale = this.drawHeight / (this.yaxis.max - this.yaxis.min);
+	
+	var html = '';
+	
+	var ticsEvery = Math.ceil(this.yaxis.max-this.yaxis.min)/(this.options.yaxis.tics-1);
+	
+	for (var t = 0; t < this.options.yaxis.tics; t++)
+	{
+		var v = t*ticsEvery;
+		var y = this.getY(v);
+		if (y >= (0-this.options.padding.top))
+		{
+			html += '<div style="top: '+y+'px;">'+v+'</div>';
+			
+			if (this.options.yaxis.ticStroke !== false)
+			{
+				this.canvas.beginPath();
+				this.canvas.moveTo(this.options.padding.left, y);
+				this.canvas.lineTo(this.options.padding.left + this.drawWidth, y);
+				this.canvas.stroke();
+				this.canvas.closePath();
+			}
+			if (this.options.yaxis.ticFill !== false && (t%2) && t > 0)
+			{
+				var y2 = this.getY((t-1)*ticsEvery);
+				this.canvas.fillRect(this.options.padding.left, y, this.drawWidth, y2-y);
+			}
+		}
+	}
+	
+	this.labels.update(html);
+}
+lineGraph.prototype.getBetterMaxY = function(y)
+{
+	while (y%(this.options.yaxis.tics-1))
+	{
+		y++;
+	}
+	return y;
+}
 
 lineGraph.prototype.getX = function(x)
 {
@@ -134,6 +187,10 @@ lineGraph.prototype.render = function()
 	
 	this.prepareLines();
 	
+	if (this.options.yaxis.tics !== false)
+	{
+		this.renderTics();
+	}
 	
 	this.lines.each(function(l)
 	{
@@ -167,13 +224,14 @@ lineGraph.prototype.render = function()
 			this.canvas.lineTo(this.getX(first.x), this.options.renderHeight);
 			this.canvas.fill();
 		}
+		this.canvas.closePath();
 		
 		//this.canvas.globalCompositeOperation = 'destination-out';
 		
 	}.bind(this));
 	
 	
-	if (this.options.padding.top || this.options.padding.bottom || this.options.padding.left || this.options.padding.right)
+	if (this.options.padding.clear && (this.options.padding.top || this.options.padding.bottom || this.options.padding.left || this.options.padding.right))
 	{
 		this.canvas.clearRect(0, 0, this.options.renderWidth, this.options.padding.top);
 		this.canvas.clearRect(0, 0, this.options.padding.left, this.options.renderHeight);
@@ -183,172 +241,6 @@ lineGraph.prototype.render = function()
 	
   	this.canvas.restore();
 };
-
-
-
-/*
-
-function lineGraph(graph, options)
-{
-	this.graph = graph;
-	this.options = options;
-	
-	this.height = options.height ? options.height : 100;
-	this.width = options.width ? options.width : 320;
-	this.paddingLeft = options.paddingLeft ? options.paddingLeft : 0;
-	this.paddingTop = options.paddingTop ? options.paddingTop : 0;
-	this.paddingBottom = options.paddingBottom ? options.paddingBottom : 0;
-	this.paddingRight = options.paddingRight ? options.paddingRight : 0;
-	this.leftScale = options.leftScale ? options.leftScale : false;
-	this.bottomScale = options.bottomScale ? options.bottomScale : false;
-	this.topValue = options.topValue ? options.topValue : 0;
-	this.bottomValue = options.bottomValue ? options.bottomValue : 999999;
-	
-	this.canvas = this.graph.getContext('2d');
-	
-	this.defaultLine =
-	{
-		strokeStyle: "rgba(0, 0, 0, 1)",
-		fillStyle: false,
-		lineWidth: 2
-	};
-	
-	this.lines = []; 
-	
-};
-
-lineGraph.prototype.changeDimenstions = function(width, height)
-{
-	this.height = height;
-	this.width = width;
-}
-lineGraph.prototype.resetTopBottomValue = function()
-{
-	this.topValue = 0;
-	this.bottomValue = 999999;
-}
-
-lineGraph.prototype.setLine = function(data, options)
-{
-	if (!options.top || !options.bottom)
-	{
-		var top = 0;
-		var bottom = 999999;
-		
-		for (var d = 0; d < data.length; d++)
-		{
-			if (data[d] !== false)
-			{
-				if (top < data[d].value) top = data[d].value;
-				if (bottom > data[d].value) bottom = data[d].value;
-			}
-		}
-	
-		if (this.topValue < top) this.topValue = top;
-		if (this.bottomValue > bottom) this.bottomValue = bottom;
-	}
-	else
-	{
-		if (this.topValue < options.top) this.topValue = options.top;
-		if (this.bottomValue > options.bottom) this.bottomValue = options.bottom;
-	}
-	
-	
-	
-	var tmpLine =
-	{
-		data: data,
-		vertical:
-		{
-			top: options.top ? options.top : top,
-			bottom: options.bottom ? options.bottom : bottom
-		},
-		strokeStyle: options.strokeStyle ? options.strokeStyle : this.defaultLine.strokeStyle,
-		fillStyle: options.fillStyle ? options.fillStyle : this.defaultLine.fillStyle,
-		lineWidth: options.lineWidth ? options.lineWidth : this.defaultLine.lineWidth
-	};
-	
-	if (this.topValue == this.bottomValue)
-		this.topValue++;
-	
-	this.lines.push(tmpLine);
-}
-lineGraph.prototype.clearLines = function()
-{
-	this.lines = [];
-}
-
-lineGraph.prototype.scaleData = function()
-{
-}
-
-lineGraph.prototype.render = function()
-{
-  	this.canvas.clearRect(0, 0, 480, 480);
-	this.canvas.save();
-	
-	this.graphHeight = (this.height - (this.paddingTop + this.paddingBottom));
-	this.graphWidth = (this.width - (this.paddingLeft + this.paddingRight));
-	
-	this.scaleData();
-	
-	for (var line = 0; line < this.lines.length; line++)
-	{
-		this.canvas.strokeStyle = this.lines[line].strokeStyle;
-		this.canvas.fillStyle = this.lines[line].fillStyle;
-		this.canvas.lineWidth = this.lines[line].lineWidth;
-		
-		var segmentLength = this.graphWidth / (this.lines[line].data.length-1);
-		
-		var first = false
-		var start = 0;
-		while (first === false && start < this.lines[line].data.length)
-		{
-			first = this.lines[line].data[start];
-			start++;
-		}
-		
-		if (start < this.lines[line].data.length)
-		{
-			this.canvas.beginPath();
-			this.canvas.moveTo(((start-1) * segmentLength) + this.paddingLeft, (this.graphHeight - (this.graphHeight / (this.topValue - this.bottomValue)) * (first.value - this.bottomValue)) + this.paddingTop);
-			
-			var last = first;
-			for (var d = start; d < this.lines[line].data.length; d++)
-			{
-				var crnt = this.lines[line].data[d];
-				if (crnt !== false)
-				{
-					this.canvas.lineTo((((d-1) * segmentLength) + segmentLength) + this.paddingLeft, (this.graphHeight - (this.graphHeight / (this.topValue - this.bottomValue)) * (crnt.value - this.bottomValue)) + this.paddingTop);
-					last = crnt;
-				}
-			}
-			
-			if (this.lines[line].fillStyle)
-			{
-				this.canvas.lineTo(this.graphWidth+5 + this.paddingLeft, (this.graphHeight - (this.graphHeight / (this.topValue - this.bottomValue)) * (last.value - this.bottomValue)) + this.paddingTop);
-				this.canvas.lineTo(this.graphWidth+5 + this.paddingLeft, this.graphHeight+5+this.paddingTop);
-				this.canvas.lineTo(((start-1) * segmentLength) + this.paddingLeft, this.graphHeight+5+this.paddingTop);
-				this.canvas.fill();
-			}
-			this.canvas.stroke();
-			
-		}
-		
-	}
-	
-	if (this.paddingLeft || this.paddingTop || this.paddingRight || this.paddingBottom)
-	{
-		this.canvas.clearRect(0, 0, this.width, this.paddingTop);
-		this.canvas.clearRect(0, 0, this.paddingLeft, this.height);
-		this.canvas.clearRect(this.width-this.paddingRight, 0, this.paddingRight, this.height);
-		this.canvas.clearRect(0, this.height-this.paddingBottom, this.width, this.paddingBottom);
-	}
-	
-  	this.canvas.restore();
-};
-
-*/
 
 
 // Local Variables:
