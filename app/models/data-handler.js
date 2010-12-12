c9e2f74e5bb074cfb0b8302bfc46bc8d34187485
oil <brandonvb@gmail.com>
2010-12-11 18:37:19 -0800

    bump to 079 for dev

--- app/models/data-handler.js
+++ app/models/data-handler.js
@@ -25,6 +25,8 @@ function dataHandlerModel()
 		'battery_scaleback_percent':	{ type: 'listPcnt', 	nice: $L("batt low thresh")	},
 		'battery_scaleback_speed':		{ type: 'listFreq',		nice: $L("batt low speed")	},
 		'override_charger':				{ type: 'toggleTF'		},
+		//'vdemand_vsel_vdd1':			{ type: 'sceneVolts',	},
+		//'vdemand_vsel_vdd2':			{ type: 'sceneVolts',	},
 	};
 	
 	this.mainAssistant = false;
